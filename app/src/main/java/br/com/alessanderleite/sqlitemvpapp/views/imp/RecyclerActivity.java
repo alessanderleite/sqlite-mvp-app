54647bcea69ecc5bde17a62a5cf6f0118cb8cc6e
alessanderleite <alessander.logistica@gmail.com>
2019-04-28 11:59:30 -0300

    create the empty activities

---
+++ app/src/main/java/br/com/alessanderleite/sqlitemvpapp/views/imp/RecyclerActivity.java
@@ -0,0 +1,15 @@
+package br.com.alessanderleite.sqlitemvpapp.views.imp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.alessanderleite.sqlitemvpapp.R;
+
+public class RecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+    }
+}

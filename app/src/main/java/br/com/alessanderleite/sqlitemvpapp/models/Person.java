7a5f710e369d29830a62ed13940ca95cc1899233
alessanderleite <alessander.logistica@gmail.com>
2019-04-29 22:49:08 -0300

    add variables, constructors, getters and setters methods inside Person class

--- app/src/main/java/br/com/alessanderleite/sqlitemvpapp/models/Person.java
+++ app/src/main/java/br/com/alessanderleite/sqlitemvpapp/models/Person.java
@@ -1,4 +1,41 @@
 package br.com.alessanderleite.sqlitemvpapp.models;
 
 public class Person {
+
+    Integer id;
+    String name;
+    String address;
+
+    public Person() {
+
+    }
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }

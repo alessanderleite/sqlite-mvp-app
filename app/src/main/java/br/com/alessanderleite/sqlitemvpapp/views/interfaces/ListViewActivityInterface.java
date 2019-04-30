953842416be4b3ec4e99e96736ae9786a7d60b36
alessanderleite <alessander.logistica@gmail.com>
2019-04-30 18:56:16 -0300

    code the ListViewActivityInterface

--- app/src/main/java/br/com/alessanderleite/sqlitemvpapp/views/interfaces/ListViewActivityInterface.java
+++ app/src/main/java/br/com/alessanderleite/sqlitemvpapp/views/interfaces/ListViewActivityInterface.java
@@ -1,4 +1,16 @@
 package br.com.alessanderleite.sqlitemvpapp.views.interfaces;
 
+import java.util.ArrayList;
+
+import br.com.alessanderleite.sqlitemvpapp.models.Person;
+
 public interface ListViewActivityInterface {
+
+    void errorData(String exception);
+
+    String[] getnamePerson();
+
+    ArrayList<Person> getListPerson();
+
+    void setListPerson();
 }

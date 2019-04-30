378b804bf26fe36c498445f542437133a144afaf
alessanderleite <alessander.logistica@gmail.com>
2019-04-30 18:51:12 -0300

    code the interface MainActivityInterface

--- app/src/main/java/br/com/alessanderleite/sqlitemvpapp/views/interfaces/MainActivityInterface.java
+++ app/src/main/java/br/com/alessanderleite/sqlitemvpapp/views/interfaces/MainActivityInterface.java
@@ -1,4 +1,16 @@
 package br.com.alessanderleite.sqlitemvpapp.views.interfaces;
 
+import java.util.ArrayList;
+
+import br.com.alessanderleite.sqlitemvpapp.models.Person;
+
 public interface MainActivityInterface {
+
+    void errorData(String exception);
+
+    String[] getNamePerson();
+
+    ArrayList<Person> getListPerson();
+
+    void setListPerson();
 }

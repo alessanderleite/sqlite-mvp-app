f4ba4af935c661cfe17936faaafd380ba2ae0d33
alessanderleite <alessander.logistica@gmail.com>
2019-04-30 18:40:21 -0300

    code the interface named PersonPresenterInterface

--- app/src/main/java/br/com/alessanderleite/sqlitemvpapp/presenters/PersonPresenterInterface.java
+++ app/src/main/java/br/com/alessanderleite/sqlitemvpapp/presenters/PersonPresenterInterface.java
@@ -1,4 +1,12 @@
 package br.com.alessanderleite.sqlitemvpapp.presenters;
 
 public interface PersonPresenterInterface {
+
+    void addPersonAction(String name, String address);
+
+    void deletePersonAction(int id);
+
+    void updatePersonAction(int id, String name, String address);
+
+    void viewAllRegisteredAction();
 }

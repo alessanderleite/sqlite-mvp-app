f9f8a7f4a0c2d7f05a77bf0e0dff4171855d71b0
alessanderleite <alessander.logistica@gmail.com>
2019-05-01 16:00:52 -0300

    code the class PersonPresenterImp

--- app/src/main/java/br/com/alessanderleite/sqlitemvpapp/presenters/PersonPresenterImp.java
+++ app/src/main/java/br/com/alessanderleite/sqlitemvpapp/presenters/PersonPresenterImp.java
@@ -1,4 +1,53 @@
 package br.com.alessanderleite.sqlitemvpapp.presenters;
 
-public class PersonPresenterImp {
+import android.content.Context;
+
+import java.util.ArrayList;
+
+import br.com.alessanderleite.sqlitemvpapp.infrastructure.helper.DatabaseHelper;
+import br.com.alessanderleite.sqlitemvpapp.models.Person;
+import br.com.alessanderleite.sqlitemvpapp.views.interfaces.MainActivityInterface;
+
+public class PersonPresenterImp implements PersonPresenterInterface {
+
+    private MainActivityInterface mainActivity;
+    DatabaseHelper dbHelper;
+
+    public PersonPresenterImp(MainActivityInterface main) {
+        this.mainActivity = main;
+    }
+
+    @Override
+    public void addPersonAction(String name, String address) {
+
+        Person person = new Person(name, address);
+        try {
+            dbHelper = new DatabaseHelper((Context) mainActivity);
+            dbHelper.addPerson(person);
+            mainActivity.addPersonSuccess();
+        } catch (Exception e) {
+            mainActivity.addPersonError(e.getMessage());
+        }
+    }
+
+    @Override
+    public void deletePersonAction(int id) {
+
+    }
+
+    @Override
+    public void updatePersonAction(int id, String name, String address) {
+
+    }
+
+    @Override
+    public void viewAllRegisteredAction() {
+        dbHelper = new DatabaseHelper((Context) mainActivity);
+        ArrayList<Person> arrayList = dbHelper.getAll();
+
+        if (arrayList.size() > 0) {
+            Person person = arrayList.get(2);
+            System.out.println(person.getName()+ "," + person.getAddress());
+        }
+    }
 }

451659cae61b26e8e5a79817c6f2f5e2d5defc2d
alessanderleite <alessander.logistica@gmail.com>
2019-05-02 21:13:54 -0300

    code the class RecyclerActivity

--- app/src/main/java/br/com/alessanderleite/sqlitemvpapp/views/imp/RecyclerActivity.java
+++ app/src/main/java/br/com/alessanderleite/sqlitemvpapp/views/imp/RecyclerActivity.java
@@ -1,9 +1,11 @@
 package br.com.alessanderleite.sqlitemvpapp.views.imp;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.com.alessanderleite.sqlitemvpapp.R;
+import br.com.alessanderleite.sqlitemvpapp.views.imp.fragments.MemberFragment;
 
 public class RecyclerActivity extends AppCompatActivity {
 
@@ -11,5 +13,20 @@ public class RecyclerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+
+        Fragment fragment = getSupportFragmentManager().findFragmentById(android.R.id.content);
+
+        if (fragment == null) {
+            fragment = MemberFragment.newInstance();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(android.R.id.content, fragment, "")
+                    .commit();
+        } else {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .attach(fragment)
+                    .commit();
+        }
     }
 }
